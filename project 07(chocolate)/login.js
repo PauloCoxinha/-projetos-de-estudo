00e393f682d51e5ae2043694236039ffdcf6229e
Paulo Kositis <153947949+PauloCoxinha@users.noreply.github.com>
2025-04-19 19:41:49 -0300

    Responsividade do site terminada
    Hoje eu fiz grande parte da responsividade do site, colocando em diversos tamanhos de telas diferentes e mudando o fundo da tela de login e register (que consequentemente se tornara a sign up), assim faltando apenas alguns detalhes para terminar que no caso é a responsividade das partes principais dos produtos (coisa que eu quero testar com slider pra ver como fica) e é basicamente isso que eu fiz hj, até amanhã o site provavelmente tá finalizado e responsivo. Não tem um banco de dados e não tem nenhum server com node.js pois o github n suporta ambos, Sendo assim para projetos futuros irei começar a estudar firebase para poder colocar os projetos com banco de dados e servers em node.js

--- project 07(chocolate)/login.js
+++ project 07(chocolate)/login.js
@@ -5,3 +5,8 @@ togglepassword.addEventListener('click', () => {
     const ispassword = passwordinput.type === "password";
     passwordinput.type = ispassword ? "text" : "password"; 
 })
+
+const botaodamaria = document.querySelector('.botaodamaria').addEventListener('click', () => {
+    window.location.href = 'index.html';
+})
+
